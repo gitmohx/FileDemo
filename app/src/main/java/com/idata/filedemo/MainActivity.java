c5d0d34935163e9149ee891618978a69d9dfca5b
mohx <17688538576@163.com>
2020-06-16 17:19:25 +0800

    init

--- app/src/main/java/com/idata/filedemo/MainActivity.java
+++ app/src/main/java/com/idata/filedemo/MainActivity.java
@@ -1,25 +1,17 @@
 package com.idata.filedemo;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static String TAG = "MainActivity";
-    private Button mWriteFile;
-    private Button mReadFile;
-    private Button mDeleteFile;
-    private EditText mEditText;
-    private TextView mTvShow;
-    @SuppressLint("SdCardPath")
-    private String filePatch = "/sdcard/test/test.txt";
+    private Button mGotoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,60 +19,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         initView();
     }
-    private void initView(){
-        mWriteFile = findViewById(R.id.button_write);
-        mEditText = findViewById(R.id.et_text);
-        mTvShow = findViewById(R.id.tv_read);
-        mReadFile = findViewById(R.id.button_read);
-        mDeleteFile = findViewById(R.id.button_delete);
-        mWriteFile.setOnClickListener(this);
-        mDeleteFile.setOnClickListener(this);
-        mReadFile.setOnClickListener(this);
+
+    private void initView() {
+        mGotoFile = findViewById(R.id.button_file);
+        mGotoFile.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.button_read:
-                String text = FileUtils.ReadTxtFile(filePatch);
-                if (!TextUtils.isEmpty(text)) {
-                    mTvShow.setText(text);
-                }else {
-                    mTvShow.setText(null);
+        switch (v.getId()) {
+            case R.id.button_file:
+                Intent mIntent = new Intent();
+                try {
+                    mIntent.setClass(MainActivity.this, Class.forName("com.idata.filedemo.FileActivity"));
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
                 }
-                break;
-            case R.id.button_write:
-                if (mEditText.getText() != null) {
-                    String packName = mEditText.getText().toString();
-                    initData(packName);
-                    mEditText.setText("");
-                }
-                break;
-            case R.id.button_delete:
-                if (!TextUtils.isEmpty(filePatch)) {
-                    FileUtils.delete(filePatch);
-                    mTvShow.setText(null);
-                }
+                this.startActivity(mIntent);
                 break;
             default:
                 break;
         }
     }
-    private void initData(String str) {
-        String filePath = "/sdcard/test/";
-        String fileName = "test.txt";
-        FileUtils.writeTxtToFile(str, filePath, fileName);
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(!TextUtils.isEmpty(filePatch)){
-            String text = FileUtils.ReadTxtFile(filePatch);
-            if (!TextUtils.isEmpty(text)) {
-                mTvShow.setText(text);
-            }else {
-                mTvShow.setText(null);
-            }
-        }
     }
 }
